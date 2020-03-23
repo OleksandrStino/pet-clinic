08bb07e42882c6586a04296c53bc656701d11095
ostino <sashastinio@gmail.com>
2020-03-23 20:56:14 +0200

    Added Index Page And Controller. close #8.

---
+++ pet-clinic-web/src/main/java/ostino/springframework/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package ostino.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
