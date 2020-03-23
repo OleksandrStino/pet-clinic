3a2637ab19acd8f8cfaa9d37c669658d4c157ab2
ostino <sashastinio@gmail.com>
2020-03-23 19:50:31 +0200

    Refactored Service Interfaces. close #10.

--- pet-clinic-data/src/main/java/ostino/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ostino/springframework/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package ostino.springframework.petclinic.services;
 
 import ostino.springframework.petclinic.models.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/ostino/springframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ostino/springframework/petclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package ostino.springframework.petclinic.services;
 
-import ostino.springframework.petclinic.models.Vet;
+        import ostino.springframework.petclinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
