d5b964f2dd17f4fc0ec4d8d40beb634fa4a285e5
ostino <sashastinio@gmail.com>
2020-04-19 16:09:21 +0300

    display list of vets. close #13.

--- pet-clinic-web/src/main/java/ostino/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ostino/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package ostino.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ostino.springframework.petclinic.services.VetService;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @GetMapping({"/vets", "/vets/index", "/vets/index.html" })
-    public String listOfVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping({"", "/", "/index", "/index.html" })
+    public String listOfVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
