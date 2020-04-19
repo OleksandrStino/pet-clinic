2957cd446f6d5779664c11692915b4761f39dacd
ostino <sashastinio@gmail.com>
2020-04-19 15:39:54 +0300

    display list of owners. close #14.

--- pet-clinic-web/src/main/java/ostino/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ostino/springframework/petclinic/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package ostino.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ostino.springframework.petclinic.services.OwnerService;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @GetMapping({"/owners", "/owners/index", "/owners/index.html" })
-    public String listOfOwners() {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping({"", "/", "/index", "/index.html" })
+    public String listOfOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 
